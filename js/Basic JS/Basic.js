209bfaef036e5c8592c15f81091adbe9bc180b34
Rudresh Oza <ozarudresh98@gmail.com>
2023-05-26 09:47:40 +0530

    js added

---
+++ js/Basic JS/Basic.js
@@ -0,0 +1,43 @@
+console.log("HELLO WORLD");
+//  how to declare a variable in JS
+// we re-declare that variable.
+// we can re assigned value of that variable.
+// we can call that variable before it is declared.
+
+// console.log(myName);
+// var myName = "Rudresh";
+// console.log(myName);
+// var myName = "Aditya";
+// myName = "Amit";
+
+// we cannot redeclare variable create by let keyword
+// we can re assigned value to let variable
+// we can't call that variable before its initialization
+
+// console.log(myLastName);
+// let myLastName = "Oza";
+// console.log(myLastName);
+// myLastName = "Sharma";
+// let myLastName = "Sharma";
+
+// we can't redeclare variable
+// we can't re assigned value to variable
+// we can't call that variable before initialization
+
+// console.log(myPrefix);
+// const myPrefix = "Mr";
+// const myPrefix = "Mrs"
+// myPrefix = "Mrs";
+
+// hoisting in js
+// console.log(myName);
+// var myName = "Rahul";
+// console.log("----------------------------");
+// console.log(myName);
+
+// let & const
+
+// console.log(myName);
+console.log(myName);
+var myName = "Rajesh";
+console.log("----------------");
