6823629c15eade2ac95291c0aa559263d4a3652c
Rudresh Oza <ozarudresh98@gmail.com>
2023-06-02 09:44:17 +0530

    My First commit ot my first react app

---
+++ ReactJS/myfirstapp/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
